c0edc5629d13c898ec85aeb87652bbb432c67737
erwin <erwin@waki.mobi>
2024-09-05 14:50:50 +0700

    cape bjir

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
-// import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>eScooter</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<title>eScooter</title>
+				<link rel="icon" href="/images/icon.png" type="image/gif" sizes="16x16" />
+				<meta content="text/html;charset=utf-8" http-equiv="Content-Type" />
+				<meta content="width=device-width, initial-scale=1.0" name="viewport" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,23 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+				<meta name="description" content="Discover the latest electric scooters for a sustainable and fast urban commute. Compare models, reviews, and find the perfect eScooter for you." />
+				<meta name="keywords" content="eScooter, electric scooter, urban mobility, sustainable transport, scooter reviews, best eScooters" />
+				<meta name="author" content="Your Company Name" />
+
+				<meta property="og:title" content="eScooter - Sustainable Urban Commute" />
+				<meta property="og:description" content="Explore the best electric scooters designed for a greener and faster urban commute." />
+				<meta property="og:image" content="https://escooterbali.com/images/favicon.png" />
+				<meta property="og:url" content="https://escooterbali.com/" />
+				<meta property="og:type" content="website" />
+
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:title" content="Discover the Best eScooters" />
+				<meta name="twitter:description" content="Check out our curated selection of the best electric scooters for urban mobility." />
+				<meta name="twitter:image" content="https://escooterbali.com/images/favicon.png" />
+
+				<link rel="shortcut icon" href="/images/favicon.png" type="image/x-icon" />
+
         <link rel="stylesheet" href="/styles/bootstrap.min.css" />
         <link rel="stylesheet" href="/styles/plugins.css" />
         <link rel="stylesheet" href="/styles/swiper.css" />
